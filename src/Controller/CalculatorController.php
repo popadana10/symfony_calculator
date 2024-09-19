d2532acf60ddbe5045c0ccd0b5e343059ccbc227
Dana Popa <popa.dana100@gmail.com>
2024-09-19 11:12:36 +0300

    Add calculator logic

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -3,16 +3,44 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
 class CalculatorController extends AbstractController
 {
     #[Route('/calculator', name: 'app_calculator')]
-    public function index(): Response
+    public function calculate(Request $request): Response
     {
+        $result = null;
+        $num1 = $request->query->get('num1');
+        $num2 = $request->query->get('num2');
+        $operation = $request->query->get('operation');
+
+        if ($num1 !== null && $num2 !== null && $operation !== null) {
+            switch ($operation) {
+                case 'add':
+                    $result = $num1 + $num2;
+                    break;
+                case 'subtract':
+                    $result = $num1 - $num2;
+                    break;
+                case 'multiply':
+                    $result = $num1 * $num2;
+                    break;
+                case 'divide':
+                    $result = $num2 != 0 ? $num1 / $num2 : 'Cannot divide by zero';
+                    break;
+                default:
+                    $result = 'Invalid operation';
+            }
+        }
+
         return $this->render('calculator/index.html.twig', [
-            'controller_name' => 'CalculatorController',
+            'result' => $result,
+            'num1' => $num1,
+            'num2' => $num2,
+            'operation' => $operation
         ]);
     }
 }
